fda303560fe17a3df3b7f3f238327723ac101567
theophrast <jjanos512@gmail.com>
2017-04-30 12:10:08 +0200

    Sample application upgraded

--- app/src/main/java/com/forgivingui/theophrast/numericinputedittextsample/SampleActivity.java
+++ app/src/main/java/com/forgivingui/theophrast/numericinputedittextsample/SampleActivity.java
@@ -1,11 +1,11 @@
 package com.forgivingui.theophrast.numericinputedittextsample;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.theophrast.forgivingui.numericinputedittext.ui.FloatInputEditText;
@@ -17,6 +17,9 @@ public class SampleActivity extends AppCompatActivity {
     IntegerInputEditText integerInputEditText;
     FloatInputEditText floatInputEditText;
 
+    Button bt_intInputValidation;
+    Button bt_floatInputValidation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,49 +29,42 @@ public class SampleActivity extends AppCompatActivity {
 
         integerInputEditText = (IntegerInputEditText) findViewById(R.id.et_intinput);
         floatInputEditText = (FloatInputEditText) findViewById(R.id.et_floatinput);
+
+        bt_intInputValidation = (Button) findViewById(R.id.bt_intinputvalidation);
+        bt_floatInputValidation = (Button) findViewById(R.id.bt_floatinputvalidation);
+
+        bt_intInputValidation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateIntegerInputEditText(view.getContext());
+            }
+        });
+
+        bt_floatInputValidation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                validateFloatInputEditText(view.getContext());
+            }
+        });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_sample, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    public void validateIntegerInputEditText(View v) {
+    private void validateIntegerInputEditText(Context ct) {
         boolean isValid = integerInputEditText.isValid();
-        if(isValid){
-            Toast.makeText(v.getContext(),"Value in IntegerInputEditText is valid\nValue: "+integerInputEditText.getValue(),Toast.LENGTH_LONG).show();
-        }else {
-            Toast.makeText(v.getContext(),"Value in IntegerInputEditText is INVALID",Toast.LENGTH_LONG).show();
+        if (isValid) {
+            Toast.makeText(ct, "Value in IntegerInputEditText is valid\nValue: " + integerInputEditText.getValue(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(ct, "Value in IntegerInputEditText is INVALID", Toast.LENGTH_LONG).show();
         }
-
-
     }
- public void validateFloatInputEditText(View v) {
+
+    private void validateFloatInputEditText(Context ct) {
         boolean isValid = floatInputEditText.isValid();
-        if(isValid){
-            Toast.makeText(v.getContext(),"Value in FloatInputEditText is valid\nValue: "+floatInputEditText.getValue(),Toast.LENGTH_LONG).show();
-        }else {
-            Toast.makeText(v.getContext(),"Value in FloatInputEditText is INVALID",Toast.LENGTH_LONG).show();
+        if (isValid) {
+            Toast.makeText(ct, "Value in FloatInputEditText is valid\nValue: " + floatInputEditText.getValue(), Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(ct, "Value in FloatInputEditText is INVALID", Toast.LENGTH_LONG).show();
         }
-
-
     }
 
 
